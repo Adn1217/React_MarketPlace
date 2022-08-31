ad66ba2a2ff9b6d15216abe7582561a26151ccd1
Adrian Fernández <77861592+Adn1217@users.noreply.github.com>
2022-08-30 21:06:09 -0500

    Se agrega props a componente ItemListContainer y se muestra en el nombre de cada card

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <ItemListContainer />
+      <ItemListContainer tipo="Computador"/>
       <Suggested />
       <Discounts />
       <Footer />
