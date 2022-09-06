f2507504e6179d01de3ebd67f74833398e831b23
Adrian Fernández <77861592+Adn1217@users.noreply.github.com>
2022-09-05 19:42:38 -0500

    Se agrega data en .json y se importa con fetch

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -5,13 +5,13 @@ import substractLogo from '../assets/images/minus.png'
 const ItemCount = () => {
   return (
     <div className="Item-Quantity">
-        <div clasName="col-md-3">
+        <div className="col-md-3">
             <a href="#"><img src={substractLogo} className="Substract-logo" alt="logo"/></a>
         </div>
         <div className="col-md-6">
-            <input type="text" class="form-control Item-Number" placeholder="Stock" aria-label="Stock" disabled/>
+            <input type="text" className="form-control Item-Number" placeholder="Stock" aria-label="Stock" disabled/>
         </div>
-        <div clasName="col-md-3">
+        <div className="col-md-3">
             <a href="#"><img src={sumLogo} className="Sum-logo" alt="logo"/></a>
         </div>
         {/* <a class="navbar-brand" href="#">Navbar</a> */}
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import pcImage from '../assets/images/pcImage.jpg';
 import ItemCount from './ItemCount';
 
+let stockDataApi = 'stock.json';
+
+
 const ItemListContainer = ({tipo}) => {
+    let data = []
+
+    useEffect( () => {
+        async function dofetch(stockDataApi){
+            setTimeout( async () => {
+                try{
+                    let response = await fetch(stockDataApi);  
+                    // let response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
+                    data = await response.json();
+                    console.log(JSON.stringify(data))
+                    // return data;
+                }catch(error){
+                    console.log("Ha ocurrido el siguiente error: ", error)
+                    // return error;
+                }
+                finally{
+                    console.log("Se realizó consulta de inventario.") 
+                }
+            },2000)
+        }
+        dofetch(stockDataApi);
+    }, [])
+
   return (
     <div className="container imgContainer">
         <div className="card">
@@ -22,7 +48,7 @@ const ItemListContainer = ({tipo}) => {
         <div className="card">
             <a href="#"><img src={pcImage} className="card-img-top" alt="pcImage3"/></a>
             <div className="card-body">
-            <p className="card-text"><strong>Nombre: </strong>{tipo} 3<br/><strong>Marca: </strong>LENOVO<br/> <strong>Año: </strong>20<br/><strong>Precio: </strong> $950.000 COP</p>
+            <p className="card-text"><strong>Nombre: </strong>{tipo} 3<br/><strong>Marca: </strong>LENOVO<br/> <strong>Año: </strong>2019<br/><strong>Precio: </strong> $950.000 COP</p>
             <ItemCount />
             </div>
         </div>
