0f4854cf4d31c63c1ec199dd2ac6a008b556a072
Adrian Fernández <77861592+Adn1217@users.noreply.github.com>
2022-09-05 23:56:11 -0500

    Se implementa contador en ItemListContainer

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import sumLogo from '../assets/images/plus.png'
 import substractLogo from '../assets/images/minus.png'
 
-const ItemCount = () => {
+const ItemCount = ({Nombre, Marca, Anio, Precio, Cantidad}) => {
+    // let Cantidad = props.Cantidad;
+    
+    const [seleccionados, setSeleccionados] = useState(0);
+    const [SumarOff, setSumarOff] = useState(true);
+    const [RestarOff, setRestarOff] = useState(true);
+    const [clase, setClase] = useState("col-md-3")
+
+    
+    useEffect (() => {
+        setSumarOff(!(Cantidad !== undefined && Cantidad>0));
+        setearClase(SumarOff);
+    }, [Cantidad])
+
+    function setearClase (obj) {
+        setClase("col-md-3" + Boolean(obj) * " opacidad");
+    }
+    
+    function CambiarSeleccionados (num) {
+        setSeleccionados(seleccionados + num);
+    }
+    // document.getElemenById("NumeroAComprar").value = seleccionados;
+
+    function Sumar () {
+        if(Cantidad > 0 && seleccionados < Cantidad){
+            CambiarSeleccionados(1);
+            setRestarOff(false);
+            setSumarOff(seleccionados + 1 == Cantidad ? true : false);
+            setClase(SumarOff);
+            setClase(RestarOff);
+        }else{
+            setSumarOff(true);
+            setClase(SumarOff);
+        }
+    }
+     
+    function Restar () {
+        console.log(seleccionados)
+        if(seleccionados > 0){
+            CambiarSeleccionados(-1);
+            setSumarOff(false);
+            setClase(SumarOff);
+            setRestarOff(seleccionados - 1 == 0 ? true : false);
+            setClase(RestarOff);
+        }else{
+            setRestarOff(true);
+            setClase(RestarOff);
+        }
+    }
   return (
     <div className="Item-Quantity">
-        <div className="col-md-3">
-            <a href="#"><img src={substractLogo} className="Substract-logo" alt="logo"/></a>
+        <div id="Sumar" className={clase} onClick={Restar} >
+            <button href="#" disabled={RestarOff}><img src={substractLogo} className="Substract-logo" alt="logo"/></button>
         </div>
         <div className="col-md-6">
-            <input type="text" className="form-control Item-Number" placeholder="Stock" aria-label="Stock" disabled/>
+            <input  id="NumeroAComprar" type="text" className="form-control Item-Number" placeholder="Stock" aria-label="Stock" value = {seleccionados} disabled/>
         </div>
-        <div className="col-md-3">
-            <a href="#"><img src={sumLogo} className="Sum-logo" alt="logo"/></a>
+        <div id="Restar" className={clase} onClick={Sumar}>
+            <button href="#" disabled={SumarOff}><img src={sumLogo} className="Sum-logo" alt="logo"/></button>
         </div>
         {/* <a class="navbar-brand" href="#">Navbar</a> */}
     </div>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,55 +1,58 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import pcImage from '../assets/images/pcImage.jpg';
 import ItemCount from './ItemCount';
 
 let stockDataApi = 'stock.json';
 
-
 const ItemListContainer = ({tipo}) => {
-    let data = []
+
+    const [stock, setStock] = useState([]);
 
     useEffect( () => {
-        async function dofetch(stockDataApi){
+        async function doFetch(stockDataApi){
             setTimeout( async () => {
                 try{
                     let response = await fetch(stockDataApi);  
                     // let response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
-                    data = await response.json();
+                    let data = await response.json();
                     console.log(JSON.stringify(data))
-                    // return data;
+                    setStock(data);
+                    return data;
                 }catch(error){
                     console.log("Ha ocurrido el siguiente error: ", error)
-                    // return error;
+                    return error;
                 }
                 finally{
                     console.log("Se realizó consulta de inventario.") 
                 }
             },2000)
-        }
-        dofetch(stockDataApi);
-    }, [])
 
+        }
+        doFetch(stockDataApi);
+    }, [])
+    console.log(stock.length>0 ? stock: "No hay datos"); 
+    // let body1 = `<p className="card-text"><strong>Nombre: </strong>${tipo} 1<br/><strong>Marca: </strong>${stock[0]["Marca"]??}<br/> <strong>Año: </strong>${stock[0]["Anio"]??}<br/><strong>Precio: </strong> $${stock[0]["Precio"]??}</p`
   return (
     <div className="container imgContainer">
         <div className="card">
             <a href="#"><img src={pcImage} className="card-img-top" alt="pcImage1"/></a>
             <div className="card-body">
-                <p className="card-text"><strong>Nombre: </strong>{tipo} 1<br/><strong>Marca: </strong>ACER<br/> <strong>Año: </strong>2021<br/><strong>Precio: </strong> $1'350.000 COP</p>
-                <ItemCount />
+                <p className="card-text">`<strong>Nombre: </strong>{tipo} 1<br/><strong>Marca: </strong><br/> <strong>Año: </strong>2021<br/><strong>Precio: </strong> $1'350.000 COP`</p>
+                <ItemCount {...stock[0]} />
             </div>
         </div>
         <div className="card">
             <a href="#"><img src={pcImage} className="card-img-top" alt="pcImage2"/></a>
             <div className="card-body">
             <p className="card-text"><strong>Nombre: </strong>{tipo} 2<br/><strong>Marca: </strong>ASUS<br/> <strong>Año: </strong>2020<br/><strong>Precio: </strong> $1'100.000 COP</p>
-            <ItemCount />
+            <ItemCount {...stock[1]}/>
             </div>
         </div>
         <div className="card">
             <a href="#"><img src={pcImage} className="card-img-top" alt="pcImage3"/></a>
             <div className="card-body">
             <p className="card-text"><strong>Nombre: </strong>{tipo} 3<br/><strong>Marca: </strong>LENOVO<br/> <strong>Año: </strong>2019<br/><strong>Precio: </strong> $950.000 COP</p>
-            <ItemCount />
+            <ItemCount {...stock[2]}/>
             </div>
         </div>
     </div>
