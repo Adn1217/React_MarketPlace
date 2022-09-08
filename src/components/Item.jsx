c72d64c39f3e259970a2b0575c17739971bd2134
Adrian Fernández <77861592+Adn1217@users.noreply.github.com>
2022-09-07 19:58:14 -0500

    Refactor de ItemListContainer e implementacion de componente Item

---
+++ src/components/Item.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import pcImage from '../assets/images/pcImage.jpg';
+import ItemCount from './ItemCount';
+
+const Item = ({tipo, Nombre, Anio, Marca, Precio, Cantidad}) => {
+
+    console.log("Item: ",tipo, Nombre, Anio, Marca, Precio, Cantidad)
+
+  return (
+    <div className="card">
+        <a href="#"><img src={pcImage} className="card-img-top" alt="pcImage1"/></a>
+        <div className="card-body">
+            <p className="card-text">
+                <strong>Tipo:</strong> {tipo}<br/> 
+                <strong>Marca: </strong>{Marca}<br/>
+                <strong>Nombre: </strong>{Nombre}<br/>
+                <strong>Año: </strong>{Anio}<br/>
+                <strong>Precio: </strong> ${Precio}</p>
+            <ItemCount stock={Cantidad} />
+        </div>
+    </div>
+  );
+}
+
+export default Item;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import pcImage from '../assets/images/pcImage.jpg';
+import Item from './Item';
 import ItemCount from './ItemCount';
-
 let stockDataApi = 'stock.json';
 
 const ItemListContainer = ({tipo}) => {
@@ -25,36 +24,20 @@ const ItemListContainer = ({tipo}) => {
                 finally{
                     console.log("Se realizó consulta de inventario.") 
                 }
-            },2000)
-
+            },1000)
         }
         doFetch(stockDataApi);
-    }, [])
-    console.log(stock.length>0 ? stock: "No hay datos"); 
-    // let body1 = `<p className="card-text"><strong>Nombre: </strong>${tipo} 1<br/><strong>Marca: </strong>${stock[0]["Marca"]??}<br/> <strong>Año: </strong>${stock[0]["Anio"]??}<br/><strong>Precio: </strong> $${stock[0]["Precio"]??}</p`
+        console.log(stock.length>0 ? stock: "No hay datos"); 
+    }
+    , []);
+
   return (
     <div className="container imgContainer">
-        <div className="card">
-            <a href="#"><img src={pcImage} className="card-img-top" alt="pcImage1"/></a>
-            <div className="card-body">
-                <p className="card-text">`<strong>Nombre: </strong>{tipo} 1<br/><strong>Marca: </strong><br/> <strong>Año: </strong>2021<br/><strong>Precio: </strong> $1'350.000 COP`</p>
-                <ItemCount {...stock[0]} />
-            </div>
-        </div>
-        <div className="card">
-            <a href="#"><img src={pcImage} className="card-img-top" alt="pcImage2"/></a>
-            <div className="card-body">
-            <p className="card-text"><strong>Nombre: </strong>{tipo} 2<br/><strong>Marca: </strong>ASUS<br/> <strong>Año: </strong>2020<br/><strong>Precio: </strong> $1'100.000 COP</p>
-            <ItemCount {...stock[1]}/>
-            </div>
-        </div>
-        <div className="card">
-            <a href="#"><img src={pcImage} className="card-img-top" alt="pcImage3"/></a>
-            <div className="card-body">
-            <p className="card-text"><strong>Nombre: </strong>{tipo} 3<br/><strong>Marca: </strong>LENOVO<br/> <strong>Año: </strong>2019<br/><strong>Precio: </strong> $950.000 COP</p>
-            <ItemCount {...stock[2]}/>
-            </div>
-        </div>
+            {stock?.map((item) => 
+                <ul key={item.Nombre}>
+                    <Item tipo={tipo} {...item}/>
+                </ul>
+            )}
     </div>
   );
 }
