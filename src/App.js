4bbb84856ca14b4cf809ad04888b2fb117bac602
Adrian Fernández <77861592+Adn1217@users.noreply.github.com>
2022-09-21 12:20:15 -0500

    Se implementa CartContext sin funcionalidad

--- src/App.js
+++ src/App.js
@@ -8,25 +8,28 @@ import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import Cart from './components/Cart';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import CartProvider from './components/CartContext';
 
 function App() {
 
   const [selectedItem, setSelectedItem] = useState();
-
+  
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes >
-          <Route exact path='/' element={<ItemListContainer setSelectedItem={setSelectedItem} />} />
-          <Route exact path='/category/:categoryId' element={<ItemListContainer setSelectedItem={setSelectedItem} />} />
-          <Route exact path='/item/:id' element={<ItemDetailContainer selectedItem={selectedItem} />} />
-          <Route exact path='/cart' element={<Cart />} />
-        </Routes>
-        <Suggested />
-        <Discounts />
-        <Footer />
-      </Router>
+      <CartProvider>
+        <Router>
+          <Header />
+          <Routes >
+            <Route exact path='/' element={<ItemListContainer setSelectedItem={setSelectedItem} />} />
+            <Route exact path='/category/:categoryId' element={<ItemListContainer setSelectedItem={setSelectedItem} />} />
+            <Route exact path='/item/:id' element={<ItemDetailContainer selectedItem={selectedItem} />} />
+              <Route exact path='/cart' element={<Cart />} />
+          </Routes>
+          <Suggested />
+          <Discounts />
+          <Footer />
+        </Router>
+      </CartProvider>
     </div>
   );
 }
